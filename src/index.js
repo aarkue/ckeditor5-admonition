e2643721aceee239fb2a9c5d184b4ffa4a52b4d8
aarkue <mail@aarkue.eu>
2022-10-27 19:18:52 +0200

    Fixed export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import admonition from './../theme/icons/admonition.svg';
 
-export { default as Admonition } from './admonition';
+export { default as Admonition} from './admonition';
+export {AdmonitionAutoformat} from './admonition-autoformat'
 
 export const icons = {
 	admonition
